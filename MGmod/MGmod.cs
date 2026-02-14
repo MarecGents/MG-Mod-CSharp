e0e795f03e82b7bbafb49aa8ddfc6cb93d9925ea
MarecGents <marecgents_tao@sina.com>
2026-02-14 17:23:41 +0800

    update 2026.02.14

--- MGmod/MGmod.cs
+++ MGmod/MGmod.cs
@@ -18,7 +18,7 @@ public record ModMetadata : AbstractModMetadata
 	public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
 	public override List<string>? Incompatibilities { get; init; }
 	public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
-	public override string? Url { get; init; }
+    public override string? Url { get; init; } = "https://github.com/MarecGents/MG-Mod/releases/latest";
     public override bool? IsBundleMod { get; init; } = true;
 	public override string? License { get; init; } = "MIT";
 }
