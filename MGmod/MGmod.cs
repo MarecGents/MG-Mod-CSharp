4584e4d937b08ff75ce1962e810f231322f28bb6
MarecGents <marecgents_tao@sina.com>
2026-06-24 16:57:07 +0800

    update 2026.06.24

--- MGmod/MGmod.cs
+++ MGmod/MGmod.cs
@@ -14,7 +14,7 @@ public record ModMetadata : AbstractModMetadata
 	public override string Name { get; init; } = "MGMod";
 	public override string Author { get; init; } = "MarecGents";
 	public override List<string>? Contributors { get; init; }
-	public override SemanticVersioning.Version Version { get; init; } = new("0.8.5");
+	public override SemanticVersioning.Version Version { get; init; } = new("0.8.6");
 	public override SemanticVersioning.Range SptVersion { get; init; } = new("4.0.13");
 	public override List<string>? Incompatibilities { get; init; }
 	public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
